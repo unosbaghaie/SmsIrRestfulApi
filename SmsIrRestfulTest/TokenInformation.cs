c84572168a990596d3ef9f5efaf3975d5aa042e9
Younos Baghaee Moghaddam <unos.bm65@gmail.com>
2017-08-30 13:57:02 +0430

    Update TokenInformation.cs

--- SmsIrRestfulTest/TokenInformation.cs
+++ SmsIrRestfulTest/TokenInformation.cs
@@ -8,7 +8,7 @@ namespace SmsIrRestfulTest
 {
     public class TokenInformation
     {
-        public static string UserApikey { get; set; } = "726ac66dd8b5aa89333ed0ba";
-        public static string SecretKey { get; set; } = "hassannassah120130";
+        public static string UserApikey { get; set; } = "72s6ac66a6dd8b5saa898s30338563ded0ba";
+        public static string SecretKey { get; set; } = "sdsdfsdfsdff";
     }
 }
